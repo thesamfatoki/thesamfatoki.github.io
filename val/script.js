eb0b85051d08a1b49126998e32b60f4a8789a5fa
Sam <88482048+thesamfatoki@users.noreply.github.com>
2026-02-01 10:20:34 +0200

    Update script.js

--- val/script.js
+++ val/script.js
@@ -1,15 +1,14 @@
 
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "are you sure?",
+    "really sure??",
+    "are you positive?",
+    "just think about it!",
+    "if you say no, i'll be really sad...",
+    "i will be very sad...",
+    "i will be very very very sad...",
+    "ok fine, i'll stop asking...",
+    "just kidding, say yes. please? ❤️"
 ];
 
 let messageIndex = 0;
